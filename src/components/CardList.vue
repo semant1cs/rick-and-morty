44e77aeb1e70a6cbd84647bcc25d2b27278fb7a2
Petrov Mikhail <59141793+semant1cs@users.noreply.github.com>
2024-05-25 05:16:19 +0500

    добавляет пагинацию по скроллу, стилизацию остальных компонентов

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="card-list">
-    <div v-for="hero in heroes">
+  <ul class="card-list">
+    <li v-for="hero in heroes">
       <CardItem :key="hero.id" :hero="hero" />
-    </div>
-  </div>
+    </li>
+  </ul>
+  <transition name="fade">
+    <div class="loading" v-show="loading"><span class="fa fa-spinner fa-spin"></span> Loading</div>
+  </transition>
 </template>
 
 <script lang="ts">
@@ -13,9 +16,26 @@ export default {
   setup() {
     return {};
   },
-  props: { heroes: { type: Object, required: true } },
+  props: { heroes: { type: Object, required: true }, handleScrollPagination: { type: Function, required: true } },
   components: {
     CardItem: CardItem,
+  },
+  data() {
+    return { loading: false };
+  },
+
+  mounted() {
+    const listElm = document.querySelector(".card-list");
+
+    listElm?.addEventListener("scroll", () => {
+      if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
+        this.loading = true;
+        this.handleScrollPagination();
+        setTimeout(() => {
+          this.loading = false;
+        }, 600);
+      }
+    });
   },
 };
 </script>
@@ -32,5 +52,32 @@ export default {
   margin-top: 70px;
   margin-left: 20px;
   row-gap: 30px;
+  list-style-type: none;
+  padding: 0;
+  overflow: auto;
+  height: 70vh;
+}
+
+.loading {
+  text-align: center;
+  color: #fff;
+  z-index: 9;
+  background-color: rgb(60, 62, 68);
+  padding: 8px 18px;
+  border-radius: 5px;
+  max-width: 70px;
+  max-height: 30px;
+  left: calc(50% - 45px);
+  top: calc(80% - 18px);
+  margin-left: 20px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="slider__btns">
-    <button @click="decrementPageIndex"><</button>
-    <button @click="incrementPageIndex">></button>
-    <span class="pages-slider" v-show="pagesCount > 0">{{ currentPageIndex }}/{{ pagesCount }}</span>
+  <div class="search-hero-tools">
+    <div class="slider__btns">
+      <button @click="decrementPageIndex"><</button>
+      <button @click="incrementPageIndex">></button>
+      <span class="pages-slider" v-show="pagesCount > 0">{{ currentPageIndex }}/{{ pagesCount }}</span>
+    </div>
+
+    <select v-model="selected" class="status-hero__select">
+      <option v-for="option in options">{{ option.value }}</option>
+    </select>
+
+    <input v-model="inputNameFilter" class="name-hero__input" />
+    <button @click="fetchCharactersInfo" class="search-heroes__btn">Поиск</button>
   </div>
 
-  <select v-model="selected">
-    <option v-for="option in options">{{ option.value }}</option>
-  </select>
-
-  <input v-model="inputNameFilter" />
-  <button @click="fetchCharactersInfo">Поиск</button>
-
-  <CardList v-if="isDataExists" :heroes="heroes" />
+  <CardList v-if="isDataExists" :heroes="heroes" :handleScrollPagination="handleScrollPagination" />
   <div v-else class="error-message">Похоже, что ничего не нашли</div>
 </template>
 
@@ -42,6 +44,7 @@ export default {
       if (this.currentPageIndex < this.pagesCount) {
         this.currentPageIndex++;
         this.fetchCharactersInfo();
+        this.scrollPageUp();
       }
     },
 
@@ -49,8 +52,15 @@ export default {
       if (this.currentPageIndex > 1) {
         this.currentPageIndex--;
         this.fetchCharactersInfo();
+        this.scrollPageUp();
       }
     },
+
+    scrollPageUp() {
+      const cardList = document.querySelector(".card-list");
+      if (cardList) cardList.scrollTop = 0;
+    },
+
     fetchCharactersInfo() {
       axios
         .get(`https://rickandmortyapi.com/api/character?page=${this.currentPageIndex}&status=${this.selected}&name=${this.inputNameFilter}`)
@@ -66,6 +76,21 @@ export default {
           this.heroes = [];
         });
     },
+    handleScrollPagination() {
+      if (this.currentPageIndex < this.pagesCount) {
+        setTimeout(() => {
+          this.currentPageIndex++;
+          axios
+            .get(`https://rickandmortyapi.com/api/character?page=${this.currentPageIndex}&status=${this.selected}&name=${this.inputNameFilter}`)
+            .then((response) => {
+              const data = response.data;
+              this.pagesCount = data.info.pages;
+              this.heroes.concat(data.results);
+              this.heroes = this.heroes.concat(data.results);
+            });
+        }, 1000);
+      }
+    },
   },
   beforeMount() {
     this.fetchCharactersInfo();
@@ -77,6 +102,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.search-hero-tools {
+  margin-left: 20px;
+}
+
 .slider__btns {
   display: flex;
   gap: 10px;
@@ -96,5 +125,44 @@ export default {
 
 .error-message {
   color: white;
+}
+
+.status-hero__select {
+  margin-top: 10px;
+}
+
+.status-hero__select,
+.name-hero__input,
+.search-heroes__btn {
+  background: rgb(60, 62, 68);
+  width: 205px;
+  height: 40px;
+  border-radius: 10px;
+  border: 1px solid transparent;
+  padding: 0;
+  padding-left: 10px;
+}
+
+.name-hero__input {
+  margin-left: 20px;
+  transition: 0.2s ease-in-out;
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    border: 1px solid white;
+    transition: 0.2s ease-in-out;
+  }
+}
+
+.search-heroes__btn {
+  margin-left: 20px;
+  color: white;
+  padding: 0;
+
+  &:hover {
+    border: 1px solid white;
+    transition: 0.2s ease-in-out;
+  }
 }
 </style>
